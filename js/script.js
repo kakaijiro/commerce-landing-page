650c1d18444b621b49e7a3ab619cd58ea7fb7cec
jirokk <kakaijiro@gmail.com>
2025-01-22 07:11:55 +0900

    Adds smooth scrolling and sticky navigation
    Implements smooth scrolling for internal links using JavaScript
    Adds sticky navigation bar functionality
    Updates navigation links to use section IDs for smooth scrolling
    
    Fixes Safari compatibility issues with smooth scrolling

--- js/script.js
+++ js/script.js
@@ -7,6 +7,47 @@ const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", () => headerEl.classList.toggle("nav-open"));
 
+// smooth scrolling animation without using the css "scroll-behavior: smooth"
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) =>
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scroll to the top
+    if (href === "#")
+      window.scrollTo({
+        top: 0, // in px value
+        behavior: "smooth",
+      });
+    // scroll to others
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    // close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  })
+);
+
+// show/hide sticky nav bar
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+    if (!ent.isIntersecting) document.body.classList.add("sticky");
+    if (ent.isIntersecting) document.body.classList.remove("sticky");
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
